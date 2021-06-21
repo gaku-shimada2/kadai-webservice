2526840b35987e33b05f6e2811184650016c4e40
Gaku <change.in2@icloud.com>
2021-06-21 14:54:52 +0000

    2nd

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,14 +7,18 @@ class PurchasesController < ApplicationController
     if @purchase.quantity == nil then
       flash.now[:danger]  = '数量を入力してください。'
       render file: "app/views/products/show.html.erb"
+    elsif @purchase.quantity < 0 && quantity > 4 then
+      flash.now[:danger]  = '数量を正しく入力してください。'
+      render file: "app/views/products/show.html.erb"
     else
+      
     session[:purchase_quantity]  = @purchase.quantity
     end
   end
 
   def create
     @purchase = current_user.purchases.build(product_id: session[:purchase_product_id], quantity: session[:purchase_quantity], payment_method: purchase_params[:payment_method])
-    session[:purchase_method] = @purchase.payment_method
+    session[:payment_method] = @purchase.payment_method
     if @purchase.save
       flash[:success] = '購入が完了しました。'
       redirect_to complete_path
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,7 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :product
+  
+  validates :quantity, presence: true, numericality: {only_integer: true, less_than: 5}
+  validates :payment_method, presence: true,inclusion: {in: %w(代引き クレジットカード)}
 end
